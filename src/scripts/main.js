078e86972ef7abe7ebbe578cce02e8b4a1c65a73
fosterion <32929118+fosterion@users.noreply.github.com>
2021-08-09 00:00:13 +0300

    Testing VueJS.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,83 @@
-const Counter = {
-    data() {
-      return {
-        counter: 0
-      }
+var app1 = new Vue({
+    el: '#wrapper',
+    data: {
+        message: 'Привет, Vue!',
+        text: 'Insomnia have a great pleasure',
+        status: true,
+        num: 1,
+        isActive: true,
+        isDivDefault: true,
+        height: 150
     }
-  }
-  
-  Vue.createApp(Counter).mount('#counter')
+});
+
+var app2 = new Vue({
+    el: '#datafunc',
+    data: {
+        list: ['one', 'two', 'three'],
+        users: [
+            { id: 1, name: 'Ivan' },
+            { id: 2, name: 'Anton' },
+            { id: 3, name: 'Vitaly' }
+        ]
+    },
+    methods: {
+        nameFunction1() {
+            alert("nameFunction1");
+        },
+        nameFunction2: function() {
+            alert("nameFunction2");
+        }
+    }
+});
+
+var app3 = new Vue({
+    el: '#some',
+    data: {
+        counter: 1
+    },
+    created: function() {
+       this.counter = 100;
+       this.increaseCounter();
+    },
+    methods: {
+        increaseCounter() {
+            this.counter++;
+        }
+    }
+});
+
+var app3 = new Vue({
+    el: '#api',
+    data: {
+        urls: {
+            hashtags: 'https://dka-develop.ru/api?type=hashtag',
+            cities: 'https://dka-develop.ru/api?type=city'
+        },
+        hashtags: [],
+        cities: [],
+        textSearch: ''
+    },
+    watch: {
+        textSearch: function() {
+            if (this.textSearch.length > 3) {
+                this.textSearch = 'Спасибо, дальше я сам';
+            }
+        }
+    },
+    created: function() {
+       
+    },
+    methods: {
+        getHashtags() {
+            axios.get(this.urls.hashtags).then((response) => {
+                this.hashtags = response.data;
+            });
+        },
+        getCities() {
+            axios.get(this.urls.cities).then((response) => {
+                this.cities = response.data;
+            });
+        }
+    }
+});
